356dc11ea415fbcc2b5d8635a0e225cbb4c9b374
Curt Caldwell and Eliot Charette <curt+eliot@>
2018-07-02 10:34:27 -0700

    Add letter with sender and recipient input form

---
+++ friend_letter.Tests/ModelsTests/friend_letterTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using friend_letter;
+
+namespace friend_letter.Tests
+{
+  [TestClass]
+  public class friend_letterTest
+  {
+    [TestMethod]
+    public void ExampleTest_True()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
